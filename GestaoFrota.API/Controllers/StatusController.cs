146ac7bd34c8c03e846234a4fd03e298f5fbfdc6
Thainá <thaina.mariano@alunos.unis.edu.br>
2026-08-17 20:10:20 -0300

    feat: adiciona endpoint de status

---
+++ GestaoFrota.API/Controllers/StatusController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+//Esse código cria GET /api/status
+
+namespace GestaoFrota.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "API funcionando",
+                projeto = "GestaoFrota"
+            });
+        }
+    }
+}
